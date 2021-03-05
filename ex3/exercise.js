51fecd060ffb052227fa242032731e6ea9e5cf2d
fithawi ghide <lgfetah@gmail.com>
2021-03-05 08:49:46 +0100

    Ex-3 done

---
+++ ex3/exercise.js
@@ -0,0 +1,17 @@
+
+ var count = 1;
+ function increase(){
+    var p_outPut = document.getElementById("outPut");
+    p_outPut.innerHTML= count
+    count++;
+   if(count == 10){
+       
+   }
+    
+}    
+function decrease(){
+  var p_outPut = document.getElementById("outPut");
+  p_outPut.innerHTML= count
+    count--;
+ 
+}
