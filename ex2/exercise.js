8e47de566334fc2947ed928eea3a020589b44dfd
fithawi ghide <lgfetah@gmail.com>
2021-03-05 08:48:57 +0100

    Ex-2 done

---
+++ ex2/exercise.js
@@ -0,0 +1,5 @@
+function windowSize() {
+    var Width = window.innerWidth;
+    var Height = window.innerHeight;
+    document.getElementById("size").innerHTML = `Width: ${Width} and Height: ${Height}`
+  }
